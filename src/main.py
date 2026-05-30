bda8b78ce230e64707de7b279feca7f53a594b52
Илья Мазохин <irmazokhin@edu.hse.ru>
2026-05-31 00:48:53 +0300

    Реализован класс Recipe.

--- src/main.py
+++ src/main.py
@@ -17,6 +17,40 @@ class Ingredient:
         if not isinstance(other, Ingredient): return NotImplemented
         return self.name == other.name and self.unit == other.unit
     
+
+class Recipe:
+    def __init__(self, title: str, ingredients: Ingredient) -> None:
+        self.title = title
+        self.ingredients = ingredients
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        if ingredient in self.ingredients:
+            for i in range(len(self.ingredients)):
+                if self.ingredients[i] == ingredient: self.ingredients[i].quantity += ingredient.quantity
+        else:
+            self.ingredients.append(ingredient)
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        if isinstance(ratio, (int,float,complex)):
+            if ratio > 0: return True
+        return False
+    def scale(self, ratio: float) -> Recipe:
+        new_recipe = Recipe(self.title, [])
+        for ingredient in self.ingredients:
+            new_ingredient = Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit)
+            new_recipe.add_ingredient(new_ingredient)
+        return new_recipe
+    def  __len__(self): return len(self.ingredients)
+    def __str__(self):
+        return self.title + " Ингридиенты:" +  ", ".join(str(ingridient) for ingridient in self.ingredients)
+    
 ing1 = Ingredient("Мука", 500.0, "г")
 ing2 = Ingredient("Мука", 203.0, "г")
-print(ing1 == ing2)
+ing3 = Ingredient("Мука", 100.0, "г")
+ingridients = []
+ingridients.append(ing1)
+recipe = Recipe("ПЛОВ", ingridients)
+recipe.add_ingredient(ing3)
+recipe1 = recipe.scale(4.0)
+print(recipe1)
+print(recipe)
+print(Recipe.is_valid_ratio(-3.14))
